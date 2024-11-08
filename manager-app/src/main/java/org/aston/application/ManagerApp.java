b513a6af89decddd56a0498f18ab73db0405eb45
SvyatoslavZL <svkv0707@mail.ru>
2024-11-08 12:30:54 +0300

    INIT COMMIT Aston-task4 SpringBoot demo app

---
+++ manager-app/src/main/java/org/aston/application/ManagerApp.java
@@ -0,0 +1,13 @@
+package org.aston.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagerApp.class, args);
+    }
+
+}
